f2f3044a0be79cae728e26cbe31111129acff636
AcceleraTOR300 <148644880+AcceleraTOR300@users.noreply.github.com>
2025-11-16 13:59:29 +0300

    Исправил шестую строку

--- Lab1/src/DialogViewerE116.java
+++ Lab1/src/DialogViewerE116.java
@@ -3,7 +3,7 @@ public class DialogViewerE116
 {
     public static void main(String[] args)
     {
-        String name = JOptionPane.showInputDialog("Как ваше Вадим?");
+        String name = JOptionPane.showInputDialog("Как ваше имя?");
         System.out.println(name);
     }
 }
