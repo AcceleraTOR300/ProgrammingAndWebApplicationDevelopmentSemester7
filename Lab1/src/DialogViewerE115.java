214c5a7c5987e3f2a207cf06ed59a76c4055be24
AcceleraTOR300 <148644880+AcceleraTOR300@users.noreply.github.com>
2025-11-20 23:13:33 +0300

    Создал файлы

--- Lab1/src/DialogViewerE115.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-public class DialogViewerE115 {
-    public static void main(String[] args) {
-        // Исходная программа
-        JOptionPane.showMessageDialog(null, "Привет, мир!");
-
-    }
-
-}
