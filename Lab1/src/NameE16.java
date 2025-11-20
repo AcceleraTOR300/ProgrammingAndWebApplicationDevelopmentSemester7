1617b67d7c406748d09cf68cb06a41bbe5963e02
AcceleraTOR300 <148644880+AcceleraTOR300@users.noreply.github.com>
2025-11-20 23:30:26 +0300

    Вот коды вернул, это первая работа которую вы дали нашей группе на 15.11.2025
    Это когда были лекция 8 ноября, вы дали чтобы попробовали сделать кода на следующую практику субботы(ну т.е. написать своё имя, нарисовать кошечек, домики и т.д.)

---
+++ Lab1/src/NameE16.java
@@ -0,0 +1,10 @@
+public class NameE16 {
+    public static void main(String[] args) {
+// Вывод имени ВАДИМ большими буквами из звёздочек
+        System.out.println("****       *          ****     *       *    *            *");
+        System.out.println("*   *     * *        *   *     *     * *    * *        * *");
+        System.out.println("***      *   *      *    *     *   *   *    *   *     *  *");
+        System.out.println("*   *    *****    *********    * *     *    *    *   *   *");
+        System.out.println("****    *     *   *       *    *       *    *      *     *");
+    }
+}
